7c64a8c206a5a8984b5ebf6860a810a63d7c3c01
MadCake <->
2019-11-21 23:18:29 +0200

    preview maximized implemented

---
+++ Assets/Resources/Scripts/UI/preview/PreviewMaximizer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PreviewMaximizer : MonoBehaviour {
+	public float ratio = 1;
+
+	Rect nonMaximizedRect;
+	Rect maximizedRect;
+	bool maximized = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float screenRatio = ((float)(Screen.width)) / Screen.height;
+
+		if (Input.GetButtonDown("Maximize preview") && Application.isPlaying)
+		{
+			maximized = !maximized;
+
+			if (!maximized)
+				Globals.instance.components.previewCamera.rect = nonMaximizedRect;
+		}
+
+		if (maximized)
+		{
+			maximizedRect.yMax = 0.95f;
+			maximizedRect.yMin = 0.05f;
+
+			maximizedRect.xMin = (float)((Screen.width - Screen.height) * 0.5 / Screen.width);
+			maximizedRect.xMax = maximizedRect.xMin + maximizedRect.height /  screenRatio * ratio;
+
+			Globals.instance.components.previewCamera.rect = maximizedRect;
+		}
+		else
+		{
+			nonMaximizedRect = Globals.instance.components.previewCamera.rect;
+			nonMaximizedRect.width = nonMaximizedRect.height /  screenRatio * ratio;
+			Globals.instance.components.previewCamera.rect = nonMaximizedRect;
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/UI/preview/PreviewTransformCopy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PreviewTransformCopy : MonoBehaviour {
+
+	public RectTransform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+		
+		Vector3 position = Globals.instance.components.previewCamera.rect.position;
+		position.x *= Screen.width;
+		position.y *= Screen.height;
+
+		Vector3 scale = Globals.instance.components.previewCamera.rect.size;
+		scale.x *= Screen.width;
+		scale.y *= Screen.height;
+
+		target.position = position;
+		target.sizeDelta = scale;
+	}
+}
